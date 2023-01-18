cb95ac9898bfe9e40b250c5b93308d16eeb9bbf4
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-01-18 10:11:15 -0500

    Initial Commit

---
+++ iAffordable/iAffordableApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAffordableApp.swift
+//  iAffordable
+//
+//  Created by Gabriel Marquez on 2023-01-18.
+//
+
+import SwiftUI
+
+@main
+struct iAffordableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
