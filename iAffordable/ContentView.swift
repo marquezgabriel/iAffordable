102ad931d1741f6bd1f41c60f964bf6ab17b5efd
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-02-20 11:06:49 -0500

    Base from WeSplit added

--- iAffordable/ContentView.swift
+++ iAffordable/ContentView.swift
@@ -8,24 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var givenName: String = ""
-    @State private var familyName: String = ""
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    let localCurrency: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currencyCode ?? "USD")
+
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        return checkAmount + tipValue
+    }
+
+    var totalPerPerson: Double {
+        grandTotal / Double(numberOfPeople + 2)
+    }
     
     var body: some View {
-        VStack {
-            TextField(
-                "Given Name",
-                text: $givenName
-            )
-            .disableAutocorrection(true)
-            TextField(
-                "Family Name",
-                text: $familyName
-            )
-            .disableAutocorrection(true)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: localCurrency)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+
+                Section {
+                    Text(grandTotal, format: localCurrency)
+                } header: {
+                    Text("Total amount")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: localCurrency)
+                } header: {
+                    Text("Amount per person")
+                }
+            }
+            .navigationTitle("iAffordable")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .textFieldStyle(.roundedBorder)
-        .padding()
     }
 }
 
