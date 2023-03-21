6ff4358bc41e88a57f109883d5b5092acc04d8fc
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-03-21 16:11:16 -0400

    UI: Minor improvements

--- iAffordable/ContentView.swift
+++ iAffordable/ContentView.swift
@@ -9,26 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var totalAnnualIncomeAfterTaxes:Double = 0.0
-    @State private var totalValueProperty:Double = 0.0
-
-    let formatter: NumberFormatter = {
-            let formatter = NumberFormatter()
-            formatter.numberStyle = .decimal
-            return formatter
-        }()
+    @State private var totalAnnualIncomeAfterTaxes:Double = 40000
+    @State private var totalValueProperty:Double = 120000
+    
+    @FocusState private var amountIsFocused: Bool
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     TextField("Total annual income - Taxes", value: $totalAnnualIncomeAfterTaxes, format: .currency(code: "USD"))
+                        .focused($amountIsFocused)
                 } header: {
-                    Text("Total annual income after Taxes")
+                    Text("Total annual income")
                         .lineLimit(1)
                 }
                 Section {
-                    TextField("Total value of the property", value: $totalValueProperty, format: .currency(code: "USD"))
+                    TextField("Property total value", value: $totalValueProperty, format: .currency(code: "USD"))
+                        .focused($amountIsFocused)
                 } header: {
                     Text("Total value of the property")
                         .lineLimit(1)
@@ -40,6 +38,14 @@ struct ContentView: View {
             .keyboardType(.decimalPad)
             .navigationTitle("iAffordable")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
