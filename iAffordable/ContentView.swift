f05696acd166e33d381429d931476e094469cf53
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-08-22 10:25:50 -0400

    Textfields clear start

--- iAffordable/ContentView.swift
+++ iAffordable/ContentView.swift
@@ -9,33 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var totalAnnualIncomeAfterTaxes: Int = 0
-    @State private var totalValueProperty: Int = 0
+    @State private var totalAnnualIncomeAfterTaxes: String? = nil
+    @State private var totalValueProperty: String? = nil
     
     @FocusState private var amountIsFocused: Bool
     
     @State private var isAffordable: Bool = true
     
+    // Computed properties
+    var totalAnnualIncomeBinding: Binding<String> {
+        Binding<String>(
+            get: { totalAnnualIncomeAfterTaxes ?? "" },
+            set: { totalAnnualIncomeAfterTaxes = $0.isEmpty ? nil : $0 }
+        )
+    }
+
+    var totalValuePropertyBinding: Binding<String> {
+        Binding<String>(
+            get: { totalValueProperty ?? "" },
+            set: { totalValueProperty = $0.isEmpty ? nil : $0 }
+        )
+    }
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Total annual income after Taxes", value: $totalAnnualIncomeAfterTaxes, format: .currency(code: "USD"))
+                    TextField("Total annual income after Taxes", text: totalAnnualIncomeBinding)
                         .focused($amountIsFocused)
                         .keyboardType(.decimalPad)
                         .onChange(of: totalAnnualIncomeAfterTaxes) { _ in
                             updateAffordability()
+                        }
+                        .onTapGesture {
+                            totalAnnualIncomeAfterTaxes = ""
                         }
                 } header: {
                     Text("Total annual income")
                         .lineLimit(1)
                 }
                 Section {
-                    TextField("Property total value", value: $totalValueProperty, format: .currency(code: "USD"))
+                    TextField("Property total value", text: totalValuePropertyBinding)
                         .focused($amountIsFocused)
                         .keyboardType(.decimalPad)
                         .onChange(of: totalValueProperty) { _ in
                             updateAffordability()
+                        }
+                        .onTapGesture {
+                            totalValueProperty = ""
                         }
                 } header: {
                     Text("Total value of the property")
@@ -43,11 +64,11 @@ struct ContentView: View {
                 }
                 Section {
                     if isAffordable {
-                        Text("👍")
+                        Text("👍✅")
                             .font(.system(size: 50))
                             .foregroundColor(.red)
                     } else {
-                        Text("👎")
+                        Text("👎🚩")
                             .font(.system(size: 50))
                             .foregroundColor(.red)
                     }
@@ -70,7 +91,10 @@ struct ContentView: View {
     }
     
     private func updateAffordability() {
-        if totalValueProperty <= (3 * totalAnnualIncomeAfterTaxes) {
+        let income = Int(totalAnnualIncomeAfterTaxes ?? "0") ?? 0
+        let propertyValue = Int(totalValueProperty ?? "0") ?? 0
+        
+        if propertyValue <= (3 * income) {
             isAffordable = true
         } else {
             isAffordable = false
